ae3a18c7447709db6f7eb29323b4ea4b30bf656e
mrasdfdela <jerryhsu830@gmail.com>
2020-08-20 22:12:49 -0400

    stock picker solution

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,1 +1,17 @@
-puts "hello world!"
+def stock_picker(arr)
+    day_picker = [0, 1]
+    largest_profit = arr[day_picker[1]] - arr[day_picker[0]]
+
+    0.upto(arr.length - 1) do |i|
+        i.upto(arr.length - 1) do |j|
+            if arr[j] - arr[i] > largest_profit
+                day_picker = [i, j]
+                largest_profit = arr[day_picker[1]] - arr[day_picker[0]]
+            end
+        end
+    end
+
+    day_picker
+end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
